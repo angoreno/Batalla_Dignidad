a4c63a266d648913f97be0d5c3b1bf8c30606c04
Isaac Pizarro C <Isaac Pizarro C@LAPTOP-MD5H2BLQ>
2020-02-27 19:00:30 -0300

    otra

--- src/dignidad/Carro.java
+++ src/dignidad/Carro.java
@@ -3,6 +3,15 @@ import java.util.*;
 
 public class Carro {
 	
+	/**
+	*
+	*@author Paula Carrasco, Javiera Dominguez, Isaac Pizarro, Emannuel Vergara
+	*@Versión 1.0
+	*
+	*Clase padre de Caguano, Kromi, Trupalla
+	*Aquí van las herencias
+	*/
+	
 	private int qPasajeros;
 	private int anoIngreso;
 	private int[][] uTablero;//ubicacion va en formato (x,Y) numerada.
